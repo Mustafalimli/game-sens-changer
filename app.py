bcd195de2ff9f7b5c4ff5b27037c22da714afb5c
Mustafa Alimli <mustafa.alimli@mf.karaelmas.edu.tr>
2024-11-28 02:39:06 +0300

    source game to desired game

--- app.py
+++ app.py
@@ -23,6 +23,19 @@ def convert_sensitivity():
     except ValueError:
         result_label.config(text="Please enter a valid number for sensitivity.")
 
-# Arayüz
+# interface
 root = tk.Tk()
 root.title("Game Sensitivity Converter")
+# source
+tk.Label(root, text="Source Game:").grid(row=0, column=0, padx=10, pady=10)
+source_game_var = tk.StringVar()
+source_game_dropdown = ttk.Combobox(root, textvariable=source_game_var)
+source_game_dropdown['values'] = list(set([key[0] for key in conversion_rates.keys()]))
+source_game_dropdown.grid(row=0, column=1, padx=10, pady=10)
+
+# desired
+tk.Label(root, text="Target Game:").grid(row=1, column=0, padx=10, pady=10)
+target_game_var = tk.StringVar()
+target_game_dropdown = ttk.Combobox(root, textvariable=target_game_var)
+target_game_dropdown['values'] = list(set([key[1] for key in conversion_rates.keys()]))
+target_game_dropdown.grid(row=1, column=1, padx=10, pady=10)
